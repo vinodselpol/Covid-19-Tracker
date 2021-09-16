821a96851cc91aad2893cc7074e4a36e95632446
vinodselpol <51135038+vinodselpol@users.noreply.github.com>
2021-09-16 18:49:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {
 import InfoBox from "./InfoBox";
 import Map from "./Map";
 import Table from "./Table";
+import { sortData, prettyPrintStat } from "./util";
 
 import LineGraph from "./LineGraph";
 import "leaflet/dist/leaflet.css";
