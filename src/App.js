4cb1f975953462961c3e1a20f7c96b58d4ffe731
vinodselpol <51135038+vinodselpol@users.noreply.github.com>
2021-09-16 18:48:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import {
-  MenuItem,
-  FormControl,
-  Select,
-  Card,
-  CardContent,
-} from "@material-ui/core";
+
 import InfoBox from "./InfoBox";
 import Map from "./Map";
 import Table from "./Table";
